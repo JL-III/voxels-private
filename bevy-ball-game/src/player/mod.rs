31f6669a33d7901f89794e29ca7d9009a7f1280e
JL-III <jesselopez1162@gmail.com>
2023-11-07 16:51:17 -0600

    fmt

--- bevy-ball-game/src/player/mod.rs
+++ bevy-ball-game/src/player/mod.rs
@@ -11,18 +11,16 @@ mod systems;
 pub struct MovementSystemSet;
 
 #[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone)]
-
 pub struct ConfinementSystemSet;
 
 pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
-        app
-        .configure_set(ConfinementSystemSet.after(MovementSystemSet))
-        .add_startup_system(spawn_player)
-        .add_system(player_movement.in_set(MovementSystemSet))
-        .add_system(confine_player_movement.in_set(ConfinementSystemSet))
+        app.configure_set(ConfinementSystemSet.after(MovementSystemSet))
+            .add_startup_system(spawn_player)
+            .add_system(player_movement.in_set(MovementSystemSet))
+            .add_system(confine_player_movement.in_set(ConfinementSystemSet))
             .add_system(enemy_hit_player)
             .add_system(player_hit_star);
     }
