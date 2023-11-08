901037a90e376be8f0f68228da2bf276bc82854c
JL-III <jesselopez1162@gmail.com>
2023-11-07 21:59:52 -0600

    fmt

--- bevy-ball-game/src/main_menu/mod.rs
+++ bevy-ball-game/src/main_menu/mod.rs
@@ -6,15 +6,13 @@ use bevy::prelude::*;
 
 use crate::AppState;
 
-use self::systems::layout::{spawn_main_menu, despawn_main_menu};
+use self::systems::layout::{despawn_main_menu, spawn_main_menu};
 
 pub struct MainMenuPlugin;
 
 impl Plugin for MainMenuPlugin {
     fn build(&self, app: &mut App) {
-        app
-        .add_system(spawn_main_menu.in_schedule(OnEnter(AppState::MainMenu)))
-        .add_system(despawn_main_menu.in_schedule(OnExit(AppState::MainMenu)));
+        app.add_system(spawn_main_menu.in_schedule(OnEnter(AppState::MainMenu)))
+            .add_system(despawn_main_menu.in_schedule(OnExit(AppState::MainMenu)));
     }
 }
-
