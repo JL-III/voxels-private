b414a534fd775627d995c18a9091c9d4463237b6
JL-III <jesselopez1162@gmail.com>
2023-11-12 15:05:14 -0600

    update color

--- voxels/src/command_interface/mod.rs
+++ voxels/src/command_interface/mod.rs
@@ -31,15 +31,20 @@ pub fn update_command_interface(
     if string.len() == 0 {
         string.push('/');
     }
-    if keyboard_input.just_pressed(KeyCode::Return) {
-        println!("Text input: {}", &*string);
-        string.clear();
-    }
-    if keyboard_input.just_pressed(KeyCode::Back) {
-        string.pop();
-    }
-    if keyboard_input.just_pressed(KeyCode::Escape) {
-        string.clear();
+    if let Some(key) = keyboard_input.get_just_pressed().next() {
+        match key {
+            KeyCode::Return => {
+                println!("Text input: {}", &*string);
+                string.clear();
+            }
+            KeyCode::Back => {
+                string.pop();
+            }
+            KeyCode::Escape => {
+                string.clear();
+            }
+            _ => {} // Handle other keys or do nothing
+        }
     }
     for ev in event_reader_char.read() {
         // ignore control (special) characters
@@ -56,7 +61,7 @@ pub fn build_command_interface(commands: &mut Commands) -> Entity {
     let command_interface_entity = commands
         .spawn((
             TextBundle {
-                background_color: BackgroundColor(Color::RED),
+                background_color: BackgroundColor(Color::GRAY),
                 style: Style {
                     width: Val::Percent(100.0),
                     position_type: PositionType::Absolute,
