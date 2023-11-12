81baffdd99e590fff1b1535222515c442e22508d
JL-III <jesselopez1162@gmail.com>
2023-11-11 18:25:04 -0600

    remove redundant call

--- voxels/src/coordinates.rs
+++ voxels/src/coordinates.rs
@@ -1,7 +1,5 @@
 use bevy::prelude::*;
 
-use crate::camera::FlyCam;
-
 pub struct CoordinatePlugin;
 
 #[derive(Component)]
@@ -10,61 +8,26 @@ pub struct CoordinateDisplay {}
 pub fn spawn_coordinate_display(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
-    flycam_query: Query<&mut Transform, With<FlyCam>>,
 ) {
-    let _main_menu_entity = build_coordinate_display(&mut commands, asset_server, flycam_query);
-}
-
-pub fn build_coordinate_display(
-    commands: &mut Commands,
-    asset_server: Res<AssetServer>,
-    flycam_query: Query<&mut Transform, With<FlyCam>>,
-) -> Entity {
-    if let Ok(flycam) = flycam_query.get_single() {
-        let coordinate_display_entity = commands
-            .spawn((
-                TextBundle {
-                    text: Text {
-                        sections: vec![TextSection::new(
-                            flycam.translation.to_string(),
-                            TextStyle {
-                                font: asset_server.load("fonts/FiraSans-Bold.ttf"),
-                                font_size: 32.0,
-                                color: Color::WHITE,
-                            },
-                        )],
-                        alignment: TextAlignment::Center,
-                        ..default()
-                    },
+    commands
+        .spawn((
+            TextBundle {
+                text: Text {
+                    sections: vec![TextSection::new(
+                        "coordinates",
+                        TextStyle {
+                            font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                            font_size: 32.0,
+                            color: Color::WHITE,
+                        },
+                    )],
+                    alignment: TextAlignment::Center,
                     ..default()
                 },
-                CoordinateDisplay {},
-            ))
-            .id();
-        coordinate_display_entity
-    } else {
-        let coordinate_display_entity = commands
-            .spawn((
-                TextBundle {
-                    text: Text {
-                        sections: vec![TextSection::new(
-                            "coordinates",
-                            TextStyle {
-                                font: asset_server.load("fonts/FiraSans-Bold.ttf"),
-                                font_size: 32.0,
-                                color: Color::WHITE,
-                            },
-                        )],
-                        alignment: TextAlignment::Center,
-                        ..default()
-                    },
-                    ..default()
-                },
-                CoordinateDisplay {},
-            ))
-            .id();
-        coordinate_display_entity
-    }
+                ..default()
+            },
+            CoordinateDisplay {},
+        ));
 }
 
 impl Plugin for CoordinatePlugin {
