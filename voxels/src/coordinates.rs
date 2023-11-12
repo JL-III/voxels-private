bc3aab82f9f84bb3fd269b5cf32bf665b4ee52a7
JL-III <jesselopez1162@gmail.com>
2023-11-11 18:58:04 -0600

    update

--- voxels/src/coordinates.rs
+++ voxels/src/coordinates.rs
@@ -5,29 +5,30 @@ pub struct CoordinatePlugin;
 #[derive(Component)]
 pub struct CoordinateDisplay {}
 
-pub fn spawn_coordinate_display(
-    mut commands: Commands,
-    asset_server: Res<AssetServer>,
-) {
-    commands
-        .spawn((
-            TextBundle {
-                text: Text {
-                    sections: vec![TextSection::new(
-                        "coordinates",
-                        TextStyle {
-                            font: asset_server.load("fonts/FiraSans-Bold.ttf"),
-                            font_size: 32.0,
-                            color: Color::WHITE,
-                        },
-                    )],
-                    alignment: TextAlignment::Center,
-                    ..default()
+pub fn get_font(asset_server: Res<AssetServer>) -> Handle<Font> {
+    asset_server.load("fonts/FiraSans-Bold.ttf")
+}
+
+pub fn get_text_bundle(asset_server: Res<AssetServer>) -> TextBundle {
+    TextBundle {
+        text: Text {
+            sections: vec![TextSection::new(
+                "coordinates",
+                TextStyle {
+                    font: get_font(asset_server),
+                    font_size: 32.0,
+                    color: Color::rgb(3.0 / 255.0, 252.0 / 255.0, 169.0 / 255.0),
                 },
-                ..default()
-            },
-            CoordinateDisplay {},
-        ));
+            )],
+            alignment: TextAlignment::Center,
+            ..default()
+        },
+        ..default()
+    }
+}
+
+pub fn spawn_coordinate_display(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn((get_text_bundle(asset_server), CoordinateDisplay {}));
 }
 
 impl Plugin for CoordinatePlugin {
